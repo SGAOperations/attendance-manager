976b633ae05ed9896fefc6a2e388cb6ca67065fb
Jake Wu-Chen <jwcgolf12@gmail.com>
2025-08-18 19:57:45 -0400

    linting

--- backend/src/app/api/middleware/auth.ts
+++ backend/src/app/api/middleware/auth.ts
@@ -1,17 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { jwtVerify, SignJWT } from "jose";
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { jwtVerify, SignJWT } from 'jose';
 
 export async function auth_middleware(request: NextRequest) {
   return await updateSession(request);
 }
 // TODO: Move to an ENV variable
-const secretKey = "secret";
+const secretKey = 'secret';
 const key = new TextEncoder().encode(secretKey);
 
 export async function login(formData: FormData) {
   // Verify credentials && get the user
-  const user = { email: formData.get("email"), name: "John" };
+  const user = { email: formData.get('email'), name: 'John' };
   // Create the session
   const expires = new Date(Date.now() + 10 * 1000);
   const session = await encrypt({ user, expires });
@@ -19,24 +19,24 @@ export async function login(formData: FormData) {
   (
     await // Save the session in a cookie
     cookies()
-  ).set("session", session, { expires, httpOnly: true });
+  ).set('session', session, { expires, httpOnly: true });
 }
 
 export async function decrypt(input: string): Promise<any> {
   const { payload } = await jwtVerify(input, key, {
-    algorithms: ["HS256"],
+    algorithms: ['HS256'],
   });
   return payload;
 }
 export async function updateSession(request: NextRequest) {
-  const session = request.cookies.get("session")?.value;
+  const session = request.cookies.get('session')?.value;
   if (!session) return;
   // Refresh the session so it doesn't expire
   const parsed = await decrypt(session);
   parsed.expires = new Date(Date.now() + 10 * 1000);
   const res = NextResponse.next();
   res.cookies.set({
-    name: "session",
+    name: 'session',
     value: await encrypt(parsed),
     httpOnly: true,
     expires: parsed.expires,
@@ -46,14 +46,14 @@ export async function updateSession(request: NextRequest) {
 
 export async function encrypt(payload: any) {
   return await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
+    .setProtectedHeader({ alg: 'HS256' })
     .setIssuedAt()
-    .setExpirationTime("10 sec from now")
+    .setExpirationTime('10 sec from now')
     .sign(key);
 }
 
 export async function getSession() {
-  const session = (await cookies()).get("session")?.value;
+  const session = (await cookies()).get('session')?.value;
   if (!session) return null;
   return await decrypt(session);
 }
@@ -63,5 +63,5 @@ export async function logout() {
   (
     await // Destroy the session
     cookies()
-  ).set("session", "", { expires: new Date(0) });
+  ).set('session', '', { expires: new Date(0) });
 }
