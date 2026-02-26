e4f00b23dadef49c8822be410413a117086a70c2
Prajwala Immareddy <pimmareddy5@gmail.com>
2026-02-25 21:56:14 -0500

    Style changes

--- attendance-manager/src/components/attendance/AttendanceMembers.tsx
+++ attendance-manager/src/components/attendance/AttendanceMembers.tsx
@@ -19,7 +19,7 @@ const AttendanceMembers: React.FC<AttendanceMembersProps> = ({
         <div className='flex items-center justify-center h-64'>
           <div className='text-center'>
             <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-[#C8102E] mx-auto mb-4'></div>
-            <p className='text-gray-600'>{"Loading attendance.."}</p>
+            <p className='text-gray-600'>{'Loading attendance..'}</p>
           </div>
         </div>
       </div>
