ce7c586c4750f9ca299cc7daf9f26004bfe5510a
Prajwala Immareddy <pimmareddy5@gmail.com>
2026-02-26 10:28:07 -0500

    Updating message

--- attendance-manager/src/components/attendance/AttendanceHistory.tsx
+++ attendance-manager/src/components/attendance/AttendanceHistory.tsx
@@ -23,7 +23,7 @@ const AttendanceHistory: React.FC<AttendanceHistoryProps> = ({
         <div className='flex items-center justify-center h-64'>
           <div className='text-center'>
             <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-[#C8102E] mx-auto mb-4'></div>
-            <p className='text-gray-600'>{'Loading attendance..'}</p>
+            <p className='text-gray-600'>Loading attendance...</p>
           </div>
         </div>
       </div>
