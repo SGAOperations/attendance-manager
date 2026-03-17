659e5757f217584b6abde6daa0170053263a263d
wongcolin45 <wongcolin45@gmail.com>
2026-03-16 20:11:53 -0400

    Added import

--- attendance-manager/src/app/api/meeting/[id]/users/route.ts
+++ attendance-manager/src/app/api/meeting/[id]/users/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server';
 import { MeetingController } from '@/meeting/meeting.controller';
 import { AttendanceController } from '@/attendance/attendance.controller';
 /**
